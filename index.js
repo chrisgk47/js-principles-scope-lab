f998c9df95da2b4f2aa8793921ce1b89e7e73abf
Christopher Kim <chris.kim1990@gmail.com>
2021-04-27 15:52:46 -0400

    scope

--- index.js
+++ index.js
@@ -2,18 +2,19 @@
 var customerName = 'bob'
 const leastFavoriteCustomer = 'bobby'
 
-function  upperCaseCustomerName(){
+function upperCaseCustomerName(){
     customerName = customerName.toUpperCase()
-}
+};
 
 function setBestCustomer(){
     bestCustomer = 'not bob'
-}
+};
 
 function overwriteBestCustomer(){
     bestCustomer = 'maybe bob'
-}
+};
 
 function changeLeastFavoriteCustomer(){
     leastFavoriteCustomer = 'bobbyo'
-}
+};
+
